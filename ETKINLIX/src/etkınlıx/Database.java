43b38b76bc6795893ee6e68e1678089decaad876
Rasit Demirel <rrasitt@gmail.com>
2013-03-28 19:06:46 +0200

    deneme

---
+++ ETKINLIX/src/etkınlıx/Database.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package etkınlıx;
+
+/**
+ *
+ * @author Rasit
+ */
+public class Database {
+    
+}
