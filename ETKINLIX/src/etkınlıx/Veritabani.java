12a0dc8eb43e70a0826c7e62572722e400db6830
Rasit Demirel <rrasitt@gmail.com>
2013-03-28 18:48:39 +0200

    veritabani Update

--- ETKINLIX/src/etkınlıx/Veritabani.java
+++ ETKINLIX/src/etkınlıx/Veritabani.java
@@ -3,20 +3,37 @@
  * and open the template in the editor.
  */
 package etkınlıx;
+
 import java.sql.*;
+
 /**
  *
  * @author ECE
  */
-
 public class Veritabani {
 
+    Statement stmt = null;
+    ResultSet rs;
+
     public Veritabani() {
+
+    
+        /*
+        
+        Class c = Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+
+        Driver drv = (Driver) c.newInstance();
+
+        conn = drv.connect("jdbc:sqlserver://159.253.37.201; databaseName=mustafa; user=mustafauser; password=mustafa2703", null);
+    
+    */
+    
+    
     }
-   
-    public void baglan() throws ClassNotFoundException, SQLException{
+
+    public void baglan() throws ClassNotFoundException, SQLException {
         Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
         String cstr = "jdbc:sqlserver://localhost;databaseName:EtkinliX";
-        Connection conn = DriverManager.getConnection(cstr,"sa","1234567?");
+        Connection conn = DriverManager.getConnection(cstr, "sa", "1234567?");
     }
 }
